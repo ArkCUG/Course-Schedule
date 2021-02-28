f0d6ce670ef2a8bc1936346ae2b2ea4512f7422e
LaTristan <50165836+LaTristan@users.noreply.github.com>
2021-02-28 23:32:16 +0800

    On branch main  Your branch is ahead of 'origin/main' by 2 commits.    (use "git push" to publish your local commits)  Changes to be committed: 	modified:   testfile.cpp

--- testfile.cpp
+++ testfile.cpp
@@ -8,5 +8,6 @@ int main(){
     cout<<"******** Course Schedule ********"<<endl;
     Course c1("空间信息可视化");
     Course c2("物联网技术与应用");
-    
+    vector<int>weeks2{1,2,3,4,5,6,7,8};
+    c2.setWeeks(weeks2);
 }
